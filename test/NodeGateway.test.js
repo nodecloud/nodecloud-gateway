08aa5fa2d5db6abd70355d953ebf5d493a869c34
yedaodao <404069912@qq.com>
2017-12-04 16:27:17 +0800

    complete test

--- test/NodeGateway.test.js
+++ test/NodeGateway.test.js
@@ -1,8 +1,40 @@
 import test from 'ava';
+import _ from 'lodash';
+import Koa from 'koa';
+import request from 'supertest';
 
 import NodeGateway from '../lib/NodeGateway';
+import DebugAbstractGatewayFilter from './filters/DebugAbstractGatewayFilter';
+import DebugPreGatewayFilter from './filters/DebugPreGatewayFilter';
+import DebugRouteGatewayFilter from './filters/DebugRouteGatewayFilter';
+import DebugPostGatewayFilter from './filters/DebugPostGatewayFilter';
+import {routeApp} from './support';
 
 test('Constructor', t => {
     let gateway = new NodeGateway();
     t.is(typeof gateway.init, 'function');
+});
+
+test.cb('init', t => {
+    const gateway = new NodeGateway();
+    const app = new Koa();
+    gateway.init(app, [
+        DebugAbstractGatewayFilter,
+        DebugPostGatewayFilter,
+        DebugRouteGatewayFilter,
+        DebugPreGatewayFilter
+    ]);
+    routeApp(app);
+    request(app.listen())
+        .get('/')
+        .expect(200)
+        .set('X-REQUIRE-GATEWAY', true)
+        .end((err, res) => {
+            if (err) {
+                throw new Error(err);
+            }
+            t.is(res.header['x-debug-gateway'], 'true');
+            t.is(res.header['x-gateway-test-order'], ',PRE,ROUTE,POST');
+            t.end();
+        });
 });
